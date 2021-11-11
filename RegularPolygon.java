7b754dc0e11e41db82e62fe16b37cafb8d56f412
Garrett Gelinas <gelinas.garrett@gmail.com>
2021-11-11 06:46:32 -0500

    Add files via upload

---
+++ RegularPolygon.java
@@ -0,0 +1,49 @@
+
+public class RegularPolygon {
+
+	public double polyCenterX;
+	public int sideNum;
+	public double sideLength;
+	public double polyCenterY;
+	
+	private int n() {
+		int sideNum = 3;
+		return 0;
+	}
+	
+	private double side() {
+		double sideLength = 1;
+		return 0;
+	}
+	
+	private double x() {
+		double polyCenterX = 0;
+		return 0;
+	}
+	private double y() {
+		double polyCenterY = 0;
+		return 0;
+	}
+	
+	RegularPolygon(){
+		
+	}
+	
+	RegularPolygon(double newSide, double newLength) {
+		
+	}
+	
+	RegularPolygon(double newSide, double newLength, double xCor, double yCor){
+		
+	}
+	
+	double getPerimeter() {
+		return 0;
+	}
+	double getArea() {
+		return 0;
+	}
+	
+	
+}
+
---
+++ testPolygon.java
@@ -0,0 +1,9 @@
+
+public class testPolygon {
+	
+	RegularPolygon poly1 = new RegularPolygon(6,4);
+	
+	
+	RegularPolygon poly2 = new RegularPolygon(10,4,5.6,7.8);
+	
+}
